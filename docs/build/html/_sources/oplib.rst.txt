6ee44baf0d35b224eb9a3e695399887e470f79aa
Kyunghwan Kim <86699249+kyunghwan-onepredict@users.noreply.github.com>
2021-12-09 12:24:34 +0900

    feat: Set sphinx which is auto documenation tool (#1)
    * feat: Set sphinx which is auto documenation tool
    
    * chore: Change pull request template to English

---
+++ docs/build/html/_sources/oplib.rst.txt
@@ -0,0 +1,16 @@
+oplib
+=====
+
+.. automodule:: oplib
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Subpackages
+-----------
+
+.. toctree::
+   :maxdepth: 4
+
+   oplib.frequency
+   oplib.time
---
+++ oplib/frequency/bye.py
@@ -0,0 +1,16 @@
+def print_bye(msg: str = "oplib"):
+    """
+    Print Bye!
+
+    Parameters
+    ----------
+    msg : str
+        Message
+
+    Examples
+    --------
+    >>> print_bye("oplib")
+    Bye, oplib!
+
+    """
+    print(f"Bye, {msg}!")
