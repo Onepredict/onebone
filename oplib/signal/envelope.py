f165792ebae9ad8a75998aa7a59378fa46a183e2
root <root@LAPTOP-852C4L77.localdomain>
2021-12-21 19:01:21 +0900

    feat: Add a function to extract envelope

---
+++ oplib/signal/envelope.py
@@ -0,0 +1,30 @@
+"""Extract envelope.
+- Author: Kangwhi Kim
+- Contact: kangwhi.kim@onepredict.com
+"""
+
+import numpy as np
+from scipy.signal import hilbert
+
+def envelope_analytic(x, axis:int = -1) -> np.ndarray:
+
+    """
+    Extract the envelope from the signal using the 'Hilbert transform'.
+
+    The transformation is done along the last axis by default.
+
+    Parameters
+    ----------
+    x : array_like
+        Signal data.  Must be real.
+    axis : int, optional
+        Axis along which to do the transformation.  Default: -1.
+
+    Returns
+    ----------
+    y: numpy.ndarray
+        Envelope of the `x`, of each 1-D array along `axis`
+    """
+    y = np.abs(hilbert(x, axis=axis))
+
+    return y
