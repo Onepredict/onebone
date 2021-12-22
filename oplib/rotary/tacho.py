c57e8c249ff39ad24e680ffbb67a0c519a77154d
kangwhi-onepredict <79968466+Kangwhi-Kim@users.noreply.github.com>
2021-12-22 16:29:21 +0900

    Feat: Add function for tacho-to-rpm and test function for that. (#3)
    * feat: Local에서 구현한 oplib 코드 추가
    
    oplib/function/convert_to_x.py
    - Tacho to RPM 과 같이 어떤 데이터를 특정한 형태로 변환하는 기능
    oplib/function/find_indices.py
    - 다른 알고리즘의 보조 기능
    oplib/function/freq_filter.py
    - 필터링 기능
    oplib/function/local_maxima_tracking.py
    - 속도추정을 위한 Local-Maxima 함수
    oplib/function/signal_basic_analysis.py
    - 간단한 시각화 기능(time, frequency, time-frequency, spectral kurtosis)
    oplib/function/test/test_a_two_step.py
    - 속도 추정하는 검증 코드 (구동)
    
    * feat: 폴더 구조 초기세팅
    
    scipy 패키지 poetry add
    
    * feat: Add tacho_to_rpm function
    
    Completed the tacho-to-rpm function for the first time.
    
    * feat: Add file to test tacho.py
    
    Add _generate_tacho_signal function, main code.
    
    * feat: Add test function to test_tacho.py
    
    Completed the test_tacho.py for the first time.
    
    * feat: Add docstring to tacho.py, test_tacho.py
    
    * feat: Add exception for tacho.py
    
    * feat: Add unittest for exception "array shape"
    
    * docs: Add author info.
    
    * docs: Modified docstrings
    
    * chore: Add author info. and modify gitmessage
    
    * chore: modify the .gitmessage.txt
    
    The contents were modified to look good to other developers.
    
    Co-authored-by: root <root@LAPTOP-852C4L77.localdomain>

---
+++ oplib/rotary/tacho.py
@@ -0,0 +1,269 @@
+"""Convert tacho to angle or RPM.
+
+- Author: Kangwhi Kim
+- Contact: kangwhi.kim@onepredict.com
+"""
+
+from typing import Tuple, Union
+
+import numpy as np
+from scipy.interpolate import interp1d
+
+
+def _get_indices_when_value_jump(x: np.ndarray, gap: float) -> np.ndarray:
+    """Find sample indices in which the sample value rises rapidly."""
+    diff_x = np.diff(x)
+    indices = np.where(diff_x >= gap)[0] + 1
+    return indices
+
+
+def _get_time_and_angles_of_each_turn(
+    x: np.ndarray,
+    fs: Union[int, float],
+    state_levels_trh: Union[int, float],
+    indices_trh: int,
+    pulses_per_rev: int,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Get time and angles of which the samples are rising."""
+    t = np.arange(x.size) / fs
+    event_indices = _get_indices_when_value_jump(x, state_levels_trh)
+    event_indices = event_indices[_get_indices_when_value_jump(event_indices, indices_trh)]
+
+    tp = t[event_indices]
+    angles = np.arange(tp.size) * (2 * np.pi) / pulses_per_rev
+
+    return tp, angles
+
+
+def tacho_to_angle(
+    x: np.ndarray,
+    fs: Union[int, float],
+    state_levels_trh: Union[int, float],
+    indices_trh: int = 2,
+    pulses_per_rev: int = 1,
+    output_fs: Union[int, float] = None,
+    fit_type: str = "linear",
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Extract angle signal from tachometer pulses.
+
+    Parameters
+    ----------
+    x: numpy.ndarray
+        Tachometer pulse signal(1-D).
+    fs: int or float
+        Sample rate.
+    state_levels_trh: int or float
+        The difference between state levels used to identify pulses.
+        (The state levels used to identify pulses.)
+    indices_trh: int
+        The difference between indices of the first samples of high-level-state of pulses. Default is 2.
+    pulses_per_rev: int
+        Number of tachometer pulses per revolution. Default is 1.
+    output_fs: int or float
+        Output sample rate. Default is `fs`.
+    fit_type: str
+        Fitting method. Default is linear.
+
+    Returns
+    -------
+    angle: numpy.ndarray
+        Rotational angle(1-D).
+    t: numpy.ndarray
+        Time(1-D) expressed in seconds.
+    tp: numpy.ndarray
+        Pulse locations(1-D) expressed in seconds.
+
+    Examples
+    --------
+    >>> x = np.array([0,1,0,1,0,0,1,0,0,1])
+    >>> fs = 10
+    >>> state_levels_trh = 0.5
+
+    >>> angle, t, tp = tacho_to_angle(x, fs, state_levels_trh)
+
+    >>> angle
+    array([-6.28318531e+00, -4.18879020e+00, -2.09439510e+00,  1.16262283e-15,
+           2.09439510e+00,  4.18879020e+00,  6.28318531e+00,  8.37758041e+00,
+           1.04719755e+01,  1.25663706e+01])
+    >>> t
+    array([0. , 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+    >>> tp
+    array([0.3, 0.6, 0.9])
+    """
+    # Set default parameter
+    if output_fs is None:
+        output_fs = fs
+
+    # Check inputs
+    if not isinstance(x, np.ndarray):
+        raise TypeError("'x' must be array.")
+    if len(x.shape) >= 2:
+        raise ValueError("'x' has less than 2 dimensions.")
+
+    if not (isinstance(fs, int) | isinstance(fs, float)):
+        raise TypeError("'fs' must be integer or float.")
+
+    if not (isinstance(state_levels_trh, int) | isinstance(state_levels_trh, float)):
+        raise TypeError("'state_levels_trh' must be integer or float.")
+
+    if not isinstance(indices_trh, int):
+        raise TypeError("'indices_trh' must be integer.")
+
+    if not isinstance(pulses_per_rev, int):
+        raise TypeError("'pulses_per_rev' must be integer.")
+
+    if not (isinstance(output_fs, int) | isinstance(output_fs, float)):
+        raise TypeError("'output_fs must' be integer or float.")
+
+    if not isinstance(fit_type, str):
+        raise TypeError("'fit_type' must be string.")
+
+    # Resample time for the sampling rate of 'output_fs'
+    inc = 1 / output_fs  # incrementals of time
+    end_time = (x.size - 1) / fs
+    t = np.arange(0, end_time + inc, inc)
+
+    # Get time and angles of each turn
+    tp, tp_angles = _get_time_and_angles_of_each_turn(
+        x, fs, state_levels_trh, indices_trh, pulses_per_rev
+    )
+
+    # Return zero if there is no rising edge
+    if tp.size == 0:
+        angle = np.zeros(t.size)
+        return angle, t, tp
+
+    # Get angle profile using interpolation between each turn
+    # TODO: which value
+    if fit_type == "linear":
+        angle_profile = interp1d(
+            tp, tp_angles, kind="linear", bounds_error=False, fill_value="extrapolate"
+        )
+    elif fit_type == "cubic":
+        angle_profile = interp1d(
+            tp, tp_angles, kind="cubic", bounds_error=False, fill_value="extrapolate"
+        )
+
+    # Get angles for resampled time
+    angle = angle_profile(t)
+
+    return angle, t, tp
+
+
+def tacho_to_rpm(
+    x: np.ndarray,
+    fs: Union[int, float],
+    state_levels_trh: Union[int, float],
+    indices_trh: int = 2,
+    pulses_per_rev: int = 1,
+    output_fs: Union[int, float] = None,
+    fit_type: str = "linear",
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Extract RPM signal from tachometer pulses.
+
+    Parameters
+    ----------
+    x: numpy.ndarray
+        Tachometer pulse signal(1-D).
+    fs: int or float
+        Sample rate.
+    state_levels_trh: int or float
+        The difference between state levels used to identify pulses.
+        (The state levels used to identify pulses.)
+    indices_trh: int
+        The difference between indices of the first samples of high-level-state of pulses. Default is 2.
+    pulses_per_rev: int
+        Number of tachometer pulses per revolution. Default is 1.
+    output_fs: int or float
+        Output sample rate. Default is `fs`.
+    fit_type: str
+        Fitting method. Default is linear.
+
+    Returns
+    -------
+    rpm: numpy.ndarray
+        Rotational speed(1-D).
+    t: numpy.ndarray
+        Time(1-D) expressed in seconds.
+    tp: numpy.ndarray
+        Pulse locations(1-D) expressed in seconds.
+
+    Examples
+    --------
+    >>> x = np.array([0,1,0,1,0,0,1,0,0,1])
+    >>> fs = 10
+    >>> state_levels_trh = 0.5
+
+    >>> rpm, t, tp = tacho_to_rpm(x, fs, state_levels_trh)
+
+    >>> rpm
+    array([200., 200., 200., 200., 200., 200., 200., 200., 200., 200.])
+    >>> t
+    array([0. , 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+    >>> tp
+    array([0.3, 0.6, 0.9])
+    """
+    # Set default parameter
+    if output_fs is None:
+        output_fs = fs
+
+    # Check inputs
+    if not isinstance(x, np.ndarray):
+        raise TypeError("'x' must be array.")
+    if len(x.shape) >= 2:
+        raise ValueError("'x' has less than 2 dimensions.")
+
+    if not (isinstance(fs, int) | isinstance(fs, float)):
+        raise TypeError("'fs' must be integer or float.")
+
+    if not (isinstance(state_levels_trh, int) | isinstance(state_levels_trh, float)):
+        raise TypeError("'state_levels_trh' must be integer or float.")
+
+    if not isinstance(indices_trh, int):
+        raise TypeError("'indices_trh' must be integer.")
+
+    if not isinstance(pulses_per_rev, int):
+        raise TypeError("'pulses_per_rev' must be integer.")
+
+    if not (isinstance(output_fs, int) | isinstance(output_fs, float)):
+        raise TypeError("'output_fs' must be integer or float.")
+
+    if not isinstance(fit_type, str):
+        raise TypeError("'fit_type' must be string.")
+
+    # Resample time for the sampling rate of 'output_fs'
+    inc = 1 / output_fs  # incrementals of time
+    end_time = (x.size - 1) / fs
+    t = np.arange(0, end_time + inc, inc)
+
+    # Get time and angles of each turn
+    tp, tp_angles = _get_time_and_angles_of_each_turn(
+        x, fs, state_levels_trh, indices_trh, pulses_per_rev
+    )
+
+    # Return zero if there is no rising edge
+    if tp.size == 0:
+        angle = np.zeros(t.size)
+        return angle, t, tp
+
+    # Calculate the RPM for each rotation section.
+    rpm_t = tp[:-1] + np.diff(tp) / 2
+    rpm = np.diff(tp_angles) / np.diff(tp) / (2 * np.pi) * 60
+
+    # Get angle profile using interpolation between each turn
+    # TODO: which value
+    if fit_type == "linear":
+        rpm_profile = interp1d(
+            rpm_t, rpm, kind="linear", bounds_error=False, fill_value="extrapolate"
+        )
+    elif fit_type == "cubic":
+        rpm_profile = interp1d(
+            rpm_t, rpm, kind="cubic", bounds_error=False, fill_value="extrapolate"
+        )
+
+    # Get RPM for resampled time
+    rpm = rpm_profile(t)
+
+    return rpm, t, tp
---
+++ tests/rotary/test_tacho.py
@@ -0,0 +1,148 @@
+"""Test code for tacho.py.
+
+- Author: Kangwhi Kim
+- Contact: kangwhi.kim@onepredict.com
+"""
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+from scipy.interpolate import interp1d
+
+from oplib.rotary.tacho import tacho_to_angle, tacho_to_rpm
+
+# TEST_PARAMS = [rpm_level, profile_type, tacho_fs, state_levels, state_levels_trh]
+TEST_PARAMS = [
+    [3600, "sine", 25600, (0, 1), 0.5],
+]
+
+# THRESHOLD is allowable error
+THRESHOLD = [0.03]
+
+
+def _generate_tacho_signal(rpm_level, profile_type, tacho_fs, state_levels=None, pulse_per_rev=1):
+    # Set default parameter
+    if state_levels is None:
+        state_levels = (0, 1)
+
+    # Generate a RPM profile
+    if profile_type == "linear":
+        t = np.linspace(0, 3, 3000)
+        rpm = rpm_level * t
+    elif profile_type == "sine":
+        t = np.linspace(0, 3, 3000)
+        rpm = rpm_level * np.sin(2 * np.pi * t) + rpm_level
+    elif profile_type == "cosine":
+        t = np.linspace(0, 3, 3000)
+        rpm = rpm_level * np.cos(2 * np.pi * t) + rpm_level
+    elif profile_type == "random":
+        t = np.linspace(0, 3, 3000)
+        rpm = rpm_level * np.random.randn(3000)
+        rpm -= np.min(rpm)
+    else:
+        raise ValueError(f"'{profile_type}' is not a valid value for profile_type")
+
+    rpm_profile = interp1d(t, rpm, kind="linear")
+
+    # Generate angle profile
+    w = rpm / 60 * (2 * np.pi)  # angle velocity
+    angles = np.cumsum(
+        0.5 * np.diff(t) * (w[1:] + w[:-1])
+    )  # TODO: It will be replaced with intergrate-function.
+    angles = np.concatenate([np.zeros(1), angles])
+    angle_profile = interp1d(t, angles, kind="linear", bounds_error=False, fill_value=0)
+
+    # Generate tacho signal
+    signal_t = np.arange(t[0], t[-1], 1 / tacho_fs)
+    signal = np.ones(signal_t.size) * state_levels[0]
+
+    angles = angle_profile(signal_t)
+    angle_per_pulse = 2 * np.pi / pulse_per_rev
+    pulse_count = angles // angle_per_pulse
+
+    if set([0, 1]) | set(np.diff(pulse_count)) != set([0, 1]):
+        raise ValueError(msg="The tacho sampling rate is too low.")
+
+    rising_indices = np.where(np.diff(pulse_count) == 1)[0] + 1
+    signal[rising_indices] = state_levels[-1]
+
+    return signal, angle_profile, rpm_profile
+
+
+def _relative_error(
+    threshold, tacho_signal, tacho_fs, state_levels_trh, angle_profile, rpm_profile, is_plot=False
+):
+    # Estimate RPM
+    angle_estimated, t, tp = tacho_to_angle(
+        tacho_signal,
+        tacho_fs,
+        state_levels_trh,
+    )
+    rpm_estimated, _, _ = tacho_to_rpm(
+        tacho_signal,
+        tacho_fs,
+        state_levels_trh,
+    )
+
+    # Relative error
+    angle_true = angle_profile(t)  # True value of rotational angle
+    angle_estimated += angle_true[
+        np.where(angle_estimated >= 0)[0][0]
+    ]  # Compensate for the phase difference between true angle and estimated angle
+    rpm_true = rpm_profile(t)  # True value of RPM
+
+    re_angle = abs(
+        (np.sum(angle_true) - np.sum(angle_estimated)) / np.sum(angle_true)
+    )  # Relative error for rotational angle
+    re_rpm = abs(
+        (np.sum(rpm_true) - np.sum(rpm_estimated)) / np.sum(rpm_true)
+    )  # Relative error for RPM
+
+    if is_plot is True:
+        _, axes = plt.subplots(1, 2)
+        axes[0].set_title("Angle Profile")
+        axes[0].set_xlabel("Time[s]")
+        axes[0].set_ylabel("Angle[rad]")
+        axes[0].plot(t, angle_true, c="b", label="real angle")
+        axes[0].scatter(t, angle_estimated, s=0.1, c="g", label="estimated angle")
+        axes[0].scatter(tp, angle_profile(tp), s=30, c="r", marker="x", label="pulse rising")
+        axes[0].legend()
+        axes[1].set_title("RPM Profile")
+        axes[1].set_xlabel("Time[s]")
+        axes[1].set_ylabel("RPM[rev/min]")
+        axes[1].plot(t, rpm_true, c="b", label="real rpm")
+        axes[1].scatter(t, rpm_estimated, s=0.1, c="g", label="estimated rpm")
+        axes[1].scatter(tp, rpm_profile(tp), s=30, c="r", marker="x", label="pulse rising")
+        axes[1].legend()
+        plt.show()
+
+    assert (
+        re_angle <= threshold
+    ), f"Wrong Angle: angle-relative error: {re_angle}, threshold: {threshold}"
+    assert re_rpm <= threshold, f"Wrong RPM: rpm-relative error: {re_rpm}, threshold: {threshold}"
+    print(f"Angle-relative error: {re_angle}\tRPM-relative error: {re_rpm}")
+
+
+def _check_array_shape(tacho_signal, tacho_fs, state_levels_trh):
+    with pytest.raises(ValueError):
+        tacho_to_angle(tacho_signal, tacho_fs, state_levels_trh)
+        tacho_to_rpm(tacho_signal, tacho_fs, state_levels_trh)
+
+
+def test_tacho(is_plot=False):
+    for (rpm_level, profile_type, tacho_fs, state_levels, state_levels_trh), threshold in zip(
+        TEST_PARAMS, THRESHOLD
+    ):
+        tacho_signal, angle_profile, rpm_profile = _generate_tacho_signal(
+            rpm_level, profile_type, tacho_fs, state_levels
+        )
+        _relative_error(
+            threshold, tacho_signal, tacho_fs, state_levels_trh, angle_profile, rpm_profile, is_plot
+        )
+        invalid_signal = np.stack([tacho_signal] * 2)
+        _check_array_shape(invalid_signal, tacho_fs, state_levels_trh)
+
+
+if __name__ == "__main__":
+    test_tacho(is_plot=False)
