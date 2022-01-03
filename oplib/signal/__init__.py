6178eeee72f1bbb4543a97461d77a7815f20921e
Kyunghwan Kim <86699249+kyunghwan-onepredict@users.noreply.github.com>
2022-01-03 21:40:59 +0900

    feat: Add scaling methods (#9)
    * feat: Implement minmax scale and zscore scale
    
    * feat: Write documentation of scaling
    
    * feat: Add path of __init__.py
    
    * feat: Add ArrayLike type hint
    
    * docs: Add example and fix typo
    
    * docs: Move default value
    
    * chore: Rollback document
    
    * fix: Add axis

--- oplib/signal/__init__.py
+++ oplib/signal/__init__.py
@@ -1,4 +1,3 @@
-from .bye import print_bye
 from .filter import bandpass_filter, bandstop_filter, highpass_filter, lowpass_filter
 
-__all__ = ["print_bye", "lowpass_filter", "highpass_filter", "bandpass_filter", "bandstop_filter"]
+__all__ = ["lowpass_filter", "highpass_filter", "bandpass_filter", "bandstop_filter"]
