8e6bfb4d4470709abf48c8d8c09a7463eb9b4a7f
Kangwhi.Kim <kangwhi.kim@onepredict.com>
2022-01-04 11:27:08 +0900

    fix: Change the code according to rule.
    The changes include the import syntax, the test-code structure of
    `test_envelope.py`, the function name.

--- oplib/signal/__init__.py
+++ oplib/signal/__init__.py
@@ -1,3 +1,10 @@
+from .envelope import envelope_hilbert
 from .filter import bandpass_filter, bandstop_filter, highpass_filter, lowpass_filter
 
-__all__ = ["lowpass_filter", "highpass_filter", "bandpass_filter", "bandstop_filter"]
+__all__ = [
+    "lowpass_filter",
+    "highpass_filter",
+    "bandpass_filter",
+    "bandstop_filter",
+    "envelope_hilbert",
+]
