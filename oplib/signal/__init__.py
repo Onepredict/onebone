a3c0310f5157f246fa8f634364be50d459de234e
Kangwhi.Kim <kangwhi.kim@onepredict.com>
2022-01-05 18:55:35 +0900

    test: Add test code for frequency_feature.py
    Tests for the mnf, mdf, vcf function when the input is the 1-D array or
    2-D arrays.

--- oplib/signal/__init__.py
+++ oplib/signal/__init__.py
@@ -1,4 +1,12 @@
-from .bye import print_bye
 from .filter import bandpass_filter, bandstop_filter, highpass_filter, lowpass_filter
+from .frequency_feature import mdf, mnf, vcf
 
-__all__ = ["print_bye", "lowpass_filter", "highpass_filter", "bandpass_filter", "bandstop_filter"]
+__all__ = [
+    "lowpass_filter",
+    "highpass_filter",
+    "bandpass_filter",
+    "bandstop_filter",
+    "mnf",
+    "mdf",
+    "vcf",
+]
---
+++ tests/signal/test_frequency_feature.py
@@ -0,0 +1,180 @@
+"""Test code for frequency_feature.py.
+
+- Author: Kangwhi Kim
+- Contact: kangwhi.kim@onepredict.com
+"""
+
+import numpy as np
+from numpy.testing import assert_almost_equal, assert_array_almost_equal
+
+from oplib.signal import mdf, mnf, vcf
+
+
+def _generate_sin_signal(amp, f, fs=None):
+    if fs is None:
+        fs = f * 2.56  # Nyquist frequency
+    t = np.arange(0, 1, 1 / fs)
+    x = amp * np.sin(2 * np.pi * f * t)
+    return x
+
+
+def check_mnf_1d_array():
+    # Get the input 1-D signal `x`.
+    fs = 100
+    x1 = _generate_sin_signal(1, 10, fs)
+    x2 = _generate_sin_signal(2, 20, fs)
+    x3 = _generate_sin_signal(1, 30, fs)
+    x_1d = x1 + x2 + x3
+
+    # Check the output when `fs` is None.
+    output = mnf(x_1d)
+    assert_almost_equal(output, 0.2)
+
+    # Check the output when `fs` is not None.
+    output = mnf(x_1d, fs)
+    assert_almost_equal(output, 20)
+
+    # Check the output when `freq_range` is not None.
+    output = mnf(x_1d, fs, freq_range=(10, 20))
+    assert_almost_equal(output, 50 / 3)
+
+
+def check_mnf_2d_arrays():
+    # Get the input 2-D signal `x`.
+    fs = 100
+    x1 = _generate_sin_signal(1, 10, fs)
+    x2 = _generate_sin_signal(2, 20, fs)
+    x3 = _generate_sin_signal(1, 30, fs)
+    x_1d = x1 + x2 + x3
+    x_2d = np.stack([x_1d, x_1d], axis=0)
+
+    # Check the output when `axis` is 0.
+    output = mnf(x_2d.T, fs=fs, axis=0)
+    assert_array_almost_equal(output, np.ones(2) * 20)
+    output = mnf(x_2d.T, fs=fs, freq_range=(10, 20), axis=0)
+    assert_array_almost_equal(output, np.ones(2) * 50 / 3)
+    output = mnf(x_2d.T, fs=fs, freq_range=(10, 20), axis=0, keepdims=True)
+    assert_array_almost_equal(output, np.ones((1, 2)) * 50 / 3)
+
+    # Check the output when `axis` is 1.
+    output = mnf(x_2d, fs=fs, axis=1)
+    assert_array_almost_equal(output, np.ones(2) * 20)
+    output = mnf(x_2d, fs=fs, freq_range=(10, 20), axis=1)
+    assert_array_almost_equal(output, np.ones(2) * 50 / 3)
+    output = mnf(x_2d, fs=fs, freq_range=(10, 20), axis=1, keepdims=True)
+    assert_array_almost_equal(output, np.ones((2, 1)) * 50 / 3)
+
+
+def check_mdf_1d_array():
+    # Get the input 1-D signal `x`.
+    fs = 100
+    x1 = _generate_sin_signal(1, 10, fs)
+    x2 = _generate_sin_signal(2, 20, fs)
+    x3 = _generate_sin_signal(1, 30, fs)
+    x_1d = x1 + x2 + x3
+
+    # Check the output when `fs` is None.
+    output = mdf(x_1d)
+    assert_almost_equal(output, 0.2, decimal=3)
+
+    # Check the output when `fs` is not None.
+    output = mdf(x_1d, fs)
+    assert_almost_equal(output, 20, decimal=3)
+
+    # Check the output when `freq_range` is not None.
+    output = mdf(x_1d, fs, freq_range=(10, 15))
+    assert_almost_equal(output, 10)
+
+
+def check_mdf_2d_arrays():
+    # Get the input 2-D signal `x`.
+    fs = 100
+    x1 = _generate_sin_signal(1, 10, fs)
+    x2 = _generate_sin_signal(2, 20, fs)
+    x3 = _generate_sin_signal(1, 30, fs)
+    x_1d = x1 + x2 + x3
+    x_2d = np.stack([x_1d, x_1d], axis=0)
+
+    # Check the output when `axis` is 0.
+    output = mdf(x_2d.T, fs=fs, axis=0)
+    assert_array_almost_equal(output, np.ones(2) * 20)
+    output = mdf(x_2d.T, fs=fs, freq_range=(10, 15), axis=0)
+    assert_array_almost_equal(output, np.ones(2) * 10)
+    output = mdf(x_2d.T, fs=fs, freq_range=(10, 15), axis=0, keepdims=True)
+    assert_array_almost_equal(output, np.ones((1, 2)) * 10)
+
+    # Check the output when `axis` is 1.
+    output = mdf(x_2d, fs=fs, axis=1)
+    assert_array_almost_equal(output, np.ones(2) * 20)
+    output = mdf(x_2d, fs=fs, freq_range=(10, 15), axis=1)
+    assert_array_almost_equal(output, np.ones(2) * 10)
+    output = mdf(x_2d, fs=fs, freq_range=(10, 15), axis=1, keepdims=True)
+    assert_array_almost_equal(output, np.ones((2, 1)) * 10)
+
+
+def check_vcf_1d_array():
+    # Get the input 1-D signal `x`.
+    fs = 100
+    x1 = _generate_sin_signal(1, 10, fs)
+    x2 = _generate_sin_signal(2, 20, fs)
+    x3 = _generate_sin_signal(1, 30, fs)
+    x_1d = x1 + x2 + x3
+
+    # Check the output when `fs` is None.
+    output = vcf(x_1d)
+    assert_almost_equal(output, 0.005)
+
+    # Check the output when `fs` is not None.
+    output = vcf(x_1d, fs)
+    assert_almost_equal(output, 50)
+
+    # Check the output when `freq_range` is not None.
+    output = vcf(x_1d, fs, freq_range=(10, 15))
+    assert_almost_equal(output, 0)
+
+
+def check_vcf_2d_arrays():
+    # Get the input 2-D signal `x`.
+    fs = 100
+    x1 = _generate_sin_signal(1, 10, fs)
+    x2 = _generate_sin_signal(2, 20, fs)
+    x3 = _generate_sin_signal(1, 30, fs)
+    x_1d = x1 + x2 + x3
+    x_2d = np.stack([x_1d, x_1d], axis=0)
+
+    # Check the output when `axis` is 0.
+    output = vcf(x_2d.T, fs=fs, axis=0)
+    assert_array_almost_equal(output, np.ones(2) * 50)
+    output = vcf(x_2d.T, fs=fs, freq_range=(10, 15), axis=0)
+    assert_array_almost_equal(output, np.zeros(2))
+    output = vcf(x_2d.T, fs=fs, freq_range=(10, 15), axis=0, keepdims=True)
+    assert_array_almost_equal(output, np.zeros((1, 2)))
+
+    # Check the output when `axis` is 1.
+    output = vcf(x_2d, fs=fs, axis=1)
+    assert_array_almost_equal(output, np.ones(2) * 50)
+    output = vcf(x_2d, fs=fs, freq_range=(10, 15), axis=1)
+    assert_array_almost_equal(output, np.zeros(2))
+    output = vcf(x_2d, fs=fs, freq_range=(10, 15), axis=1, keepdims=True)
+    assert_array_almost_equal(output, np.zeros((2, 1)))
+
+
+def test_mnf():
+    check_mnf_1d_array()
+    check_mnf_2d_arrays()
+
+
+def test_mdf():
+    check_mdf_1d_array()
+    check_mdf_2d_arrays()
+
+
+def test_vcf():
+    check_vcf_1d_array()
+    check_vcf_2d_arrays()
+
+
+if __name__ == "__main__":
+    test_mnf()
+    test_mdf()
+    test_vcf()
