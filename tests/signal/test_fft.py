0a5e3def7a4d5a1179d012a679d1b05640731414
DaeyeopNa <daeyeop.na@onepredict.com>
2022-01-06 19:43:39 +0900

    fix: delete underbar of variable name

--- tests/signal/test_fft.py
+++ tests/signal/test_fft.py
@@ -6,7 +6,7 @@ import pytest
 from oplib.signal import positive_fft
 
 
-def _generate_signal(fs: float):
+def generate_signal(fs: float):
     N = 600
     # sample spacing
     T = 1.0 / fs
@@ -15,31 +15,31 @@ def _generate_signal(fs: float):
     return signal
 
 
-def check_signal(fft_: Callable, input_args: Tuple[float, bool, bool], expected_return: np.ndarray):
+def check_signal(fft: Callable, input_args: Tuple[float, bool, bool], expected_return: np.ndarray):
     fs = input_args[0]
-    signal = _generate_signal(fs)
-    f, mag = fft_(signal, *input_args)
+    signal = generate_signal(fs)
+    f, mag = fft(signal, *input_args)
     freq = np.around(f[np.where(mag[0] > 1)])
     assert np.all(np.equal(freq, expected_return)), (
         f"Wrong return: The expected return is {expected_return}, " + f"but output is {freq}"
     )
 
 
-def check_array_shape(fft_: Callable, input_args: Tuple[float, bool, bool]):
+def check_array_shape(fft: Callable, input_args: Tuple[float, bool, bool]):
     fs = input_args[0]
-    signal = _generate_signal(fs)
+    signal = generate_signal(fs)
     changed_signal = np.stack([signal] * 3, axis=0)
     changed_signal = np.stack([changed_signal] * 2, axis=0)
     # case 1: signal shape [2, 3, data_length] -> ArrayShapeError
     with pytest.raises(ValueError) as ex:
-        fft_(changed_signal, *input_args)
+        fft(changed_signal, *input_args)
     assert str(ex.value) == "Dimension of signal must be less than 3"
 
 
-def check_hann_(fft_: Callable, input_args: Tuple[float, bool, bool], expected_return: np.ndarray):
+def check_hann(fft: Callable, input_args: Tuple[float, bool, bool], expected_return: np.ndarray):
     fs = input_args[0]
-    signal = _generate_signal(fs)
-    f, mag = fft_(signal, *input_args)
+    signal = generate_signal(fs)
+    f, mag = fft(signal, *input_args)
     freq = f[np.where(mag[0] > 2)]
     assert np.all(np.equal(freq, expected_return)), (
         f"Wrong return: The expected return is {expected_return}, " + f"but output is {freq}"
@@ -49,7 +49,7 @@ def check_hann_(fft_: Callable, input_args: Tuple[float, bool, bool], expected_r
 def test_fft():
     check_signal(positive_fft, (800.0, False, True), np.array([49.0, 51.0, 80.0]))
     check_array_shape(positive_fft, (800.0, False, True))
-    check_hann_(positive_fft, (800.0, True, True), np.array([]))
+    check_hann(positive_fft, (800.0, True, True), np.array([]))
 
 
 if __name__ == "__main__":
