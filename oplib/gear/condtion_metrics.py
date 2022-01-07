f58cea93baf8366ac78e5bd5e5aed5f65c8e3537
Kangwhi.Kim <kangwhi.kim@onepredict.com>
2022-01-07 20:33:54 +0900

    feat: Modify the logic of NA4 calculation
    The result is almost same for the Matlab.
    (https://kr.mathworks.com/help/predmaint/ref/
    gearconditionmetrics.html#d123e10535)

--- oplib/gear/condtion_metrics.py
+++ oplib/gear/condtion_metrics.py
@@ -4,21 +4,18 @@
 - Contact: kyunghwan.kim@onepredict.com, kangwhi.kim@onepredict.com
 """
 
-from typing import List, Union
+from typing import Tuple, Union
 
 import numpy as np
 from scipy.stats import moment
 
-from oplib.signal import bandstop_filter
-
 
 def na4(
-    x,
+    tsa_x_list: Tuple[np.ndarray, ...],
     fs: Union[int, float],
     rpm: float,
-    orderlist: List[float],
+    freq_list: Tuple[float] = None,
     n_harmonics: int = 2,
-    n_rotations: int = 1,
 ):
     """
 
@@ -31,8 +28,6 @@ def na4(
     n_harmonics : int, default=2
         A positive integer specifying the number of shaft
         and gear meshing frequency harmonics to remove.
-    n_rotations : int, default=1
-        A positive integer specifying the number of shaft rotations in the TSA signal X.
 
     Returns
     -------
@@ -40,48 +35,40 @@ def na4(
     Examples
     --------
     """
+    # Default hyperparameters
+    if freq_list is None:
+        freq_list = []
+    else:
+        freq_list = [f for f in freq_list]
+
     # Get residual signal
-    ## gmf 는 각 기어에 대한 mesh frequency
     rps = rpm / 60
-    gmf: List[float] = rps * np.array(orderlist)
 
-    freq_tolerance = 0.0369
+    # Filtering할 주파수 성분
+    freq_list.append(rps)
 
-    ## 각 gmf의 highest harmonics (n개)를 filtering (bandstop)
-    residual_signal = x
-    for freq_center in gmf:
-        for n in range(1, n_harmonics + 1):
-            harmonic_freq = freq_center * n
-            # NOTE: Consider freq_band argument
-            freq_band = harmonic_freq * freq_tolerance
-            l_cutoff = harmonic_freq - freq_band / 2
-            h_cutoff = harmonic_freq + freq_band / 2
-            residual_signal = bandstop_filter(residual_signal, fs, l_cutoff, h_cutoff, order=2)
-    ## rotaional speed의 highest harmonics를 filtering
-    for n in range(1, n_rotations + 1):
-        harmonic_freq = rps * n
-        freq_band = harmonic_freq * freq_tolerance
-        l_cutoff = harmonic_freq - freq_band / 2
-        h_cutoff = harmonic_freq + freq_band / 2
-        residual_signal = bandstop_filter(residual_signal, fs, l_cutoff, h_cutoff, order=2)
+    # 각 gmf의 highest harmonics (n개)를 filtering (bandstop)
+    v, na4_list = [], []
+    for x in tsa_x_list:
+        n = x.size
+        # TODO: oplib fft로 교체 고려
+        amp = np.fft.rfft(x)
+        freq = np.fft.rfftfreq(n, d=1 / fs)
+        freq_band = rps
 
-    # Caculate NA4
-    # Metrics based on ensemble of residual TSA signals
+        for freq_center in freq_list:
+            for n in range(1, n_harmonics + 1):
+                harmonic_freq = freq_center * n
+                filtered_indices = np.where(np.abs(freq - harmonic_freq) <= freq_band / 2)
+                amp[filtered_indices] = 0
+        residual_signal = np.fft.irfft(amp)
 
-    import matplotlib.pyplot as plt
-    from scipy.stats import kurtosis
+        # Caculate NA4
+        # Metrics based on ensemble of residual TSA signals
+        # TODO: NA4가 res의 kurtosis가 아닐 수 있음
+        v.append(moment(residual_signal, 2))  # Variance at current time, k
+        na4 = moment(residual_signal, 4) / np.mean(v) ** 2  # use average variance up to k
+        na4_list.append(na4)
+        # print(moment(residual_signal, 4), np.mean(v))
 
-    freq_x = np.fft.rfftfreq(x.size, 1 / fs)[:-1]
-    freq_res = np.fft.rfftfreq(residual_signal.size, 1 / fs)[:-1]
-    x_mag = abs((np.fft.rfft(x) / x.size)[:-1] * 2)
-    res_mag = abs((np.fft.rfft(residual_signal) / residual_signal.size)[:-1] * 2)
-
-    # plt.plot(freq_x, x_mag)
-    # plt.plot(freq_res, res_mag)
-    # plt.show()
-
-    v = moment(residual_signal, 2)  # Variance at current time, k
-    na4 = moment(residual_signal, 4) / np.mean(v) ** 2  # use average variance up to k
-    plt.hist(residual_signal, 200, alpha=0.3)
-
-    return na4, kurtosis(residual_signal)
+    return na4_list
--- tests/gear/test_condition_metrics.py
+++ tests/gear/test_condition_metrics.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     rpm = 180
     fs = 50e3
     t = np.arange(0, (1 / 3) - 1 / fs, 1 / fs)  # sample times
-    orderlist = [17.0, 51.0]
+    freq_list = [17.0 * 3, 51.0 * 3]
     f = rpm / 60 * np.array([1, 17, 51, 10])
 
     signal_list = []
@@ -27,25 +27,8 @@ if __name__ == "__main__":
             # gear mesh vibration for gears 5 and 6 and noise
             + (2 * (k / 6) * np.sin(2 * np.pi * 10 * f[0] * t) + np.random.randn(t.size) / 5)
         )
-        # signal_list.append((2 * (k / 6) * np.sin(2 * np.pi * 10 * f[0] * t)))
 
-    import matplotlib.pyplot as plt
-    from scipy.stats import kurtosis, moment
-
-    na4_list, k_list = [], []
-    for signal in signal_list:
-        # plt.plot(t, signal, alpha=.3)
-        # plt.hist(signal, 100, alpha=.3)
-        # plt.show()
-
-        # na4_, kurtosis = na4(signal, fs, rpm, orderlist)
-        # na4_list.append(na4_)
-        # k_list.append(kurtosis)
-        v = moment(signal, 2)  # Variance at current time, k
-        na4 = moment(signal, 4) / (np.mean(v) ** 2)  # use average variance up to k
-        na4_list.append(na4)
-        k_list.append(kurtosis(signal, fisher=False))
+    na4_list = na4(signal_list, fs, rpm, freq_list)
 
     print(na4_list)
-    print(k_list)
     ""
