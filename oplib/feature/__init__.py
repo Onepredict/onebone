7a209be023dd9c99ed07abe52d126a92cd48f80a
Kyunghwan Kim <kyunghwan.kim@onepredict.com>
2022-01-13 12:04:52 +0900

    feat: Modify version before releasing

--- oplib/feature/__init__.py
+++ oplib/feature/__init__.py
@@ -1,5 +1,5 @@
 from .frequency import mdf, mnf, vcf
-from .gear_condition_metrics import na4
+from .gear import na4
 from .time import crest_factor, kurtosis, peak2peak, rms
 
 __all__ = ["mdf", "mnf", "vcf", "peak2peak", "rms", "crest_factor", "kurtosis", "na4"]
