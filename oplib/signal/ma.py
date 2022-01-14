4ec6f61570c4f980e5f89a7ab4d4f5c4b22481b6
kbpark-onepredict <kbpark@onepredict.com>
2022-01-15 00:09:44 +0900

    [feat]: [moving average]
    [-- body --]
    moving average 구현 완료

---
+++ oplib/signal/ma.py
@@ -0,0 +1,118 @@
+"""A Moving Average (MA) which returns the weighted average of array.
+
+- Author: Kibum Park
+- Contact: kibum.park@onepredict.com
+"""
+
+from typing import Union
+
+import numpy as np
+
+
+def moving_average(
+    signal: np.ndarray,
+    window_size: Union[int, float],
+    pad: bool = False,
+    weights: Union[np.ndarray, None] = None,
+) -> np.ndarray:
+    """
+    Weighted moving average.
+
+    Parameters
+    ----------
+    signal : numpy.ndarray of shape (signal_length,), (n, signal_length,)
+        Original time-domain signal.
+    window_size : Union[int, float]
+        Window size.
+    pad : bool, default=False
+        Padding method.
+        If True, Pads with the edge values of array is added. So the shape of output is same as `signal`.
+    weights : Union[numpy.ndarray of shape (window_size,), None], optional, default=None
+        Weighting coefficients. If None, the `weights` are uniform.
+
+    Returns
+    -------
+    out : numpy.ndarray
+        Moving average signal.
+        If input shape is [signal_length,], output shape is [signal_length,].
+        If input shape is [n, signal_length,], output shape is [n, signal_length,].
+        If mode is 'valid', output shape is [signal_length - window_size + 1,].
+        If mode is 'same', output shape is [signal_length,].
+
+    Examples
+    --------
+    >>> signal = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    >>> window_size = 3
+    >>> moving_average(signal, window_size)
+    [2, 3, 4, 5, 6, 7, 8, 9]
+
+
+    >>> signal = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    >>> window_size = 3
+    >>> moving_average(signal, window_size, mode='same')
+    [1, 1.33333333, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    >>> signal = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    >>> window_size = 3
+    >>> weights = np.array([0, 1, 2])
+    >>> moving_average(signal, window_size, weights=weights)
+    [1.33333333, 2.33333333, 3.33333333, 4.33333333, 5.33333333, 6.33333333, 7.33333333, 8.33333333]
+
+    """
+    if not isinstance(signal, np.ndarray):
+        raise TypeError(f"signal must be numpy.ndarray, not {type(signal).__name__}")
+    if not (isinstance(window_size, int) or isinstance(window_size, float)):
+        raise TypeError(
+            f"Argument 'window_size' must be int or float, not {type(window_size).__name__}."
+        )
+    if not isinstance(pad, bool):
+        raise TypeError(f"Argument 'pad' must be bool, not {type(pad).__name__}.")
+    if not isinstance(weights, (np.ndarray, type(None))):
+        raise TypeError(
+            f"Argument 'weights' must be numpy.ndarray or None, not {type(weights).__name__}."
+        )
+    if len(signal.shape) == 1:
+        if signal.shape[0] < window_size:
+            raise ValueError("Length of signal must be greater than window_size.")
+    elif len(signal.shape) == 2:
+        if signal.shape[1] < window_size:
+            raise ValueError("Length of signal must be greater than window_size.")
+    if len(signal.shape) > 2:
+        raise ValueError("Dimension of signal must be less than 3.")
+    if weights is not None:
+        if weights.shape[0] != window_size:
+            raise ValueError(f"Length of weights must be {window_size}.")
+
+    if weights is None:
+        weights = np.ones(window_size) / window_size
+    else:
+        weights = weights / np.sum(weights)
+
+    ma = []
+    if len(signal.shape) == 1:
+        signal = np.expand_dims(signal, axis=0)
+    for idx in range(signal.shape[0]):
+        each_arr = signal[idx, :]
+        if pad:
+            each_arr = np.pad(each_arr, (window_size - 1, 0), mode="edge")
+        ma.append(np.convolve(each_arr, weights, mode="valid"))
+    ma = np.array(ma)
+    if signal.shape[0] == 1:
+        ma = ma.squeeze()
+
+    return ma
+
+
+if __name__ == "__main__":
+    signal = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    window_size = 3
+    print(moving_average(signal, window_size))
+
+    signal = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    window_size = 3
+    print(moving_average(signal, window_size, pad=True))
+
+    signal = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
+    window_size = 3
+    weights = np.array([0, 1, 2])
+    print(moving_average(signal, window_size, pad=True, weights=weights))
---
+++ tests/signal/test_ma.py
@@ -0,0 +1,98 @@
+"""Test Moving Average
+
+- Author: Kibum Park
+- Contact: kibum.park@onepredict.com
+"""
+
+from typing import Callable, Tuple
+
+import numpy as np
+import pytest
+
+from oplib.signal import moving_average
+
+
+def _generate_sin_signal() -> np.ndarray:
+    # Generate signal
+    signal = np.arange(1, 11)
+
+    return signal
+
+
+def check_1d_signal_default(filter_: Callable, input_args: Tuple[int], expected_return: np.ndarray):
+    signal = _generate_sin_signal()
+    ma = filter_(signal, *input_args)
+
+    assert np.all(np.isclose(ma, expected_return)), (
+        f"Wrong return: The expected return is {expected_return}, " + f"but output is {ma}"
+    )
+
+
+def check_1d_signal_with_pad(
+    filter_: Callable, input_args: Tuple[int, bool], expected_return: np.ndarray
+):
+    signal = _generate_sin_signal()
+    ma = filter_(signal, *input_args)
+
+    assert np.all(np.isclose(ma, expected_return)), (
+        f"Wrong return: The expected return is {expected_return}, " + f"but output is {ma}"
+    )
+
+
+def check_1d_signal_with_weights(
+    filter_: Callable, input_args: Tuple[int, bool, np.ndarray], expected_return: np.ndarray
+):
+    signal = _generate_sin_signal()
+    ma = filter_(signal, *input_args)
+
+    assert np.all(np.isclose(ma, expected_return)), (
+        f"Wrong return: The expected return is {expected_return}, " + f"but output is {ma}"
+    )
+
+
+def check_2d_signal_wth_all(
+    filter_: Callable, input_args: Tuple[int, bool, np.ndarray], expected_return: np.ndarray
+):
+    signal = _generate_sin_signal()
+    signal_2d = np.stack([signal] * 2)
+
+    ma = filter_(signal_2d, *input_args)
+
+    assert np.all(np.isclose(ma, expected_return)), (
+        f"Wrong return: The expected return is {expected_return}, " + f"but output is {ma}"
+    )
+
+
+def check_array_shape(filter_: Callable, input_args: Tuple[int]):
+    origin_signal = _generate_sin_signal()
+
+    # case 1: signal shape [2, 3, data_length] -> ArrayShapeError
+    changed_signal = np.stack([origin_signal] * 3, axis=0)
+    changed_signal = np.stack([changed_signal] * 2, axis=0)
+    with pytest.raises(ValueError) as ex:
+        filter_(changed_signal, *input_args)
+    assert str(ex.value) == "Dimension of signal must be less than 3."
+
+
+def test_moving_average():
+    check_1d_signal_default(
+        moving_average, (3,), np.array([2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0])
+    )
+    check_1d_signal_with_pad(
+        moving_average, (3, True), np.array([1.0, 4 / 3, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0])
+    )
+    check_1d_signal_with_weights(
+        moving_average,
+        (3, False, np.array([0, 1, 2])),
+        np.array([4, 7, 10, 13, 16, 19, 22, 25]) / 3,
+    )
+    check_2d_signal_wth_all(
+        moving_average,
+        (3, True, np.array([0, 1, 2])),
+        np.stack([np.array([3, 3, 4, 7, 10, 13, 16, 19, 22, 25]) / 3] * 2),
+    )
+    check_array_shape(moving_average, (3,))
+
+
+if __name__ == "__main__":
+    test_moving_average()
