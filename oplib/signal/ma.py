65979df42bf4c9b538fa30a69ba41928ee40c470
kbpark-onepredict <kbpark@onepredict.com>
2022-01-17 10:59:43 +0900

    [fix]: [debugging용 코드 제거](#30)
    [-- body --]

--- oplib/signal/ma.py
+++ oplib/signal/ma.py
@@ -106,10 +106,3 @@ def moving_average(
         ma = ma.squeeze()
 
     return ma
-
-
-if __name__ == "__main__":
-    signal = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    window_size = 3
-    weights = np.array([0, 1, 2])
-    print(moving_average(signal, window_size, pad=True))
