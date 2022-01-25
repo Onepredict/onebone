af0ef530c42a74536910bfce0ad25c4945264249
Kibum Park <67666211+kbpark-onepredict@users.noreply.github.com>
2022-01-25 16:40:58 +0900

    feat: Add moving average (#30)
    * [feat]: [moving average]
    
    [-- body --]
    moving average 구현 완료
    
    * [style]: [테스트용 코드 제거]
    
    [-- body --]
    ma.py에 테스트용으로 남아있던 코드 제거
    
    * [fix]: [Docstring 작성 오류 수정]
    
    [-- body --]
    
    return값이 ma인데, document에서는 out으로 작성하여 수정
    
    * [fix]: [convolution 연산에서 weight를 뒤집어서 연산해야하는 점 반영](#30)
    
    [-- body --]
    
    * [fix]: [weights가 2차원 이상일 때 에러 발생하도록 수정](#30))
    
    [-- body --]
    
    * [fix]: [Apply along axis 적용 및 코드 가독성 향상](#30)
    
    [-- body --]
    
    * [test]: [테스트 결과 수정](#30)
    
    [-- body --]
    weights를 뒤집어주는 로직을 추가하면서 테스트 결과가 잘못 나왔던 부분을
    수정
    
    * [fix]: [debugging용 코드 제거](#30)
    
    [-- body --]
    
    * docs: test_ma.py에 마침표 추가 (#30)
    
    [-- body --]
    
    * style: 테스트 과정에서 함수테스트 과정에서 함수 호출 인자 변경(#30)
    
    [-- body --]
    
    * fix: pad가 True일 때 로직 변경(#30)
    
    [-- body --]
    맨 앞의 값을 늘린 상태로 convolve 옵션을 줄 경우 평균값이 첫번째 값에
    종속성이 커지는 것을 막기 위해, 계산 되는 영역만으로 가중 평균을 구하는
    로직으로 변경
    
    * docs: 수식 추가 (#30)
    
    [-- body --]
    
    * test: test 결과 수정(#30)
    
    [-- body --]
    
    * style: test 과정에서 작성한 코드 제거(#30)
    
    [-- body --]
    
    * style: 이중 if문 and로 묶음(#30)
    
    [-- body --]
    
    * feat: numpy.convolve를 scipy.signal.convolve로 교체(#30)
    
    [-- body --]
    
    * docs: return에 반영이 안된 부분 반영(#30)
    
    [-- body --]
    
    * docs: 수식 설명 자세하게 수정(#30)
    
    [-- body --]
    
    * feat: weights와 window_size 중 하나만 들어와도 작동하도록 수정(#30)
    
    [-- body --]
    
    * feat: if 문 구조 수정(#30)
    
    [-- body --]
    
    * feat: 함수 분리 및 파일 이름 변경(#30)
    
    [-- body --]
    함수 내 함수를 함수 밖으로 빼냈습니다.
    _ma_1d -> _moving_average_1d
    typeerror, valueerror를 _moving_average_check으로 빼냈습니다. C901
    회피를 위해 변경하였습니다.
    
    추 후 smoothing으로 합쳐질 것을 대비하여 ma.py -> smoothing.py로
    변경하였습니다.
    
    * fix: 디버깅에 사용한 코드 제거(#30)
    
    [-- body --]
    
    * docs: add kbpark-onepredict as a contributor for code (#44)
    
    * feat: Add checking version (#31)
    
    * feat: Add check version
    
    * fix: Add empty line
    
    * feat: Fix github action (#32)
    
    * feat: Fix github action (#33)
    
    * docs: update README.md [skip ci]
    
    * docs: update .all-contributorsrc [skip ci]
    
    Co-authored-by: Kyunghwan Kim <86699249+kyunghwan-onepredict@users.noreply.github.com>
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    
    * docs: 함수 별 docstring 추가(#30)
    
    [-- body --]
    
    * fix: 내장 함수 convention 적용(#30)
    
    [-- body --]
    
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    Co-authored-by: Kyunghwan Kim <86699249+kyunghwan-onepredict@users.noreply.github.com>

---
+++ oplib/signal/smoothing.py
@@ -0,0 +1,170 @@
+"""A Moving Average (MA) which returns the weighted average of array.
+
+- Author: Kibum Park
+- Contact: kibum.park@onepredict.com
+"""
+
+from typing import Union
+
+import numpy as np
+from scipy.signal import convolve
+
+
+def _moving_average_1d(
+    signal_1d: np.ndarray, window_size: Union[int, float], pad: bool, weights: np.ndarray
+) -> np.ndarray:
+    """Weighted moving average of 1d signal.
+    This method is used for numpy.apply_along axis() in moving_average().
+    """
+    pad_size = window_size - 1
+
+    if pad:
+        convolve_1d = convolve(signal_1d, weights)[:-pad_size]
+        div_arr = np.concatenate(
+            [np.cumsum(weights), np.repeat(np.sum(weights), signal_1d.shape[0] - window_size)],
+            axis=0,
+        )
+        moving_average_1d = convolve_1d / div_arr
+
+    else:
+        convolve_1d = convolve(signal_1d, weights)[pad_size:-pad_size]
+        div_arr = np.repeat(np.sum(weights), signal_1d.shape[0] - window_size + 1)
+        moving_average_1d = convolve_1d / div_arr
+
+    return moving_average_1d
+
+
+def _check_moving_average_args(
+    signal: np.ndarray,
+    pad: bool,
+    window_size: Union[int, float],
+    weights: np.ndarray,
+    axis: Union[int, float],
+) -> None:
+    """Check the validity of the input parameters for moving average."""
+    if not isinstance(signal, np.ndarray):
+        raise TypeError(f"signal must be numpy.ndarray, not {type(signal).__name__}")
+    if not (isinstance(window_size, int) or isinstance(window_size, float)):
+        raise TypeError(
+            f"Argument 'window_size' must be int or float, not {type(window_size).__name__}."
+        )
+    if not isinstance(pad, bool):
+        raise TypeError(f"Argument 'pad' must be bool, not {type(pad).__name__}.")
+    if not isinstance(weights, (np.ndarray, type(None))):
+        raise TypeError(
+            f"Argument 'weights' must be numpy.ndarray or None, not {type(weights).__name__}."
+        )
+    if not isinstance(axis, int) or isinstance(axis, float):
+        raise TypeError(f"Argument 'axis' must be int or float, not {type(axis).__name__}.")
+
+    if len(signal.shape) == 1:
+        if signal.shape[0] < window_size:
+            raise ValueError("Length of signal must be greater than window_size.")
+        elif np.abs(axis) > 1:
+            raise ValueError("Axis must be smaller than signal dimmesion.")
+    elif len(signal.shape) == 2:
+        if signal.shape[1] < window_size:
+            raise ValueError("Length of signal must be greater than window_size.")
+        elif np.abs(axis) > 2:
+            raise ValueError("Axis must be smaller than signal dimmesion.")
+    elif len(signal.shape) > 2:
+        raise ValueError("Dimension of signal must be less than 3.")
+
+    if weights is not None:
+        if weights.shape[0] != window_size:
+            raise ValueError(f"Length of weights must be {window_size}.")
+        elif len(weights.shape) >= 2:
+            raise ValueError("Dimension of weights must be less than 2.")
+        elif weights[0] == 0:
+            raise ValueError("First element of weights must be non-zero. Remove first 0 element.")
+
+
+def moving_average(
+    signal: np.ndarray,
+    window_size: Union[int, float],
+    pad: bool = False,
+    weights: Union[np.ndarray, None] = None,
+    axis: Union[int, float] = -1,
+) -> np.ndarray:
+    """
+    Weighted moving average.
+    .. math:: WMA(x, w, t, n) = \\sum_{i=n-t+1}^{n} w_i x_i,
+    where :math:`x` is the input array,
+    :math:`w_i` is the weight of the :math:`i`-th element,
+    :math:`t` is the window size,
+    :math:`n` is the :math:`n`th value of the input array,
+    if pad is True and :math: `n` is smaller than :math:`t`, :math:`i` is set to :math:`0`.
+
+
+
+    Parameters
+    ----------
+    signal : numpy.ndarray of shape (signal_length,), (n, signal_length,)
+        Original time-domain signal.
+    window_size : Union[int, float], optional, default=10
+        Window size.
+        One of `window_size`,`weights` must be specified.
+    pad : bool, default=False
+        Padding method.
+        If True, Pads with the edge values of array is added. So the shape of output is same as `signal`.
+    weights : Union[numpy.ndarray of shape (window_size,), None], optional, default=None
+        Weighting coefficients.
+        If None, the `weights` are uniform.
+        One of `window_size`,`weights` must be specified.
+    axis : Union[int, float], optional, default=-1
+        The axis of the input data array along which to apply the moving average.
+
+    Returns
+    -------
+    ma : numpy.ndarray
+        Moving average signal.
+        If input shape is [signal_length,], output shape is [signal_length,].
+        If input shape is [n, signal_length,] and axis is 1, output shape is [n, signal_length,].
+        If input shape is [signal_length, n] and axis is 0, output shape is [signal_length, n].
+        If pad is False, output shape is [signal_length - window_size + 1,].
+        If pad is True, output shape is [signal_length,].
+
+    Examples
+    --------
+    >>> signal = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    >>> window_size = 3
+    >>> moving_average(signal, window_size)
+    [2, 3, 4, 5, 6, 7, 8, 9]
+
+
+    >>> signal = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    >>> window_size = 3
+    >>> moving_average(signal, window_size, pad=True)
+    [1, 1.5, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    >>> signal = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    >>> window_size = 3
+    >>> weights = np.array([1, 2, 3])
+    >>> moving_average(signal, window_size, weights=weights)
+    [2.33333333, 3.33333333, 4.33333333, 5.33333333, 6.33333333, 7.33333333, 8.33333333, 9.33333333]
+
+    """
+    _check_moving_average_args(signal, pad, window_size, weights, axis)
+
+    if window_size is None:
+        window_size = weights.shape[0]
+
+    if weights is None:
+        weights = np.ones(window_size)
+    weights = weights[::-1]
+
+    if len(signal.shape) == 1:
+        signal = np.atleast_2d(signal)
+
+    if (axis == 0) or (axis == -2):
+        signal = signal.T
+
+    ma = np.apply_along_axis(_moving_average_1d, 1, signal, *(window_size, pad, weights))
+
+    if (len(signal.shape) == 2) and ((axis == 0) or (axis == -2)):
+        ma = ma.T
+
+    if signal.shape[0] == 1:
+        ma = ma.squeeze()
+
+    return ma
