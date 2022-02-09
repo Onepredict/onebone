ab5da5249e65bf42dd5d109c7e9f32042bc95f9a
seone-kim <sewon.kim@onepredict.com>
2022-02-09 01:27:16 +0000

    feat: Reflect the comments in the pull request

--- onebone/signal/snr.py
+++ onebone/signal/snr.py
@@ -1,28 +1,32 @@
+from typing import Union
+
 import numpy as np
 from scipy import signal
 
 
-def snr_feature(x: np.ndarray, fs: float, nperseg: int = 256, noverlap: int = 32) -> np.ndarray:
+def snr(
+    x: np.ndarray, fs: Union[int, float] = 1000, nperseg: int = 256, noverlap: int = 32
+) -> np.ndarray:
     """
     Extract the SNR(Signal-to-Noise-Ratio) feature from the signal using the 'STFT'.
     SNR is the ratio between max power intensity of frequency and power of other frequencies
-    at time t in the STFT spetrum.
+    at time t in the STFT spectrum.
 
     Parameters
     ----------
-    x : array_like
-        Signal data. Must be real.
-    fs : float
+    x : numpy.ndarray
+        1d-Signal data. Must be real.
+    fs : int or float
         Sampling rate.
-    nperseg : int, optional
-        Length of each segment. Default value is 256
-    noverlap : int, optional
-        Number of points to overlap between segments. Default value is 32
+    nperseg : int, default=256
+        Length of each segment.
+    noverlap : int, default=32
+        Number of points to overlap between segments.
 
     Returns
     ----------
     snr : numpy.ndarray
-        SNR of the `x`, 1d-array
+        SNR of the `x`, 1d-array.
 
     Examples
     --------
@@ -31,7 +35,7 @@ def snr_feature(x: np.ndarray, fs: float, nperseg: int = 256, noverlap: int = 32
     >>> x = 10.0 * np.sin(2 * np.pi * 20.0 * t)
     >>> snr_array = snr_feature(x, fs)
 
-    Usage
+    Notes
     --------
     1. Get a snr array by using snr_feature for one of given signals
     2. Make the segments of snr array and get the mean of each segment.
@@ -39,11 +43,12 @@ def snr_feature(x: np.ndarray, fs: float, nperseg: int = 256, noverlap: int = 32
     e.g. SNR_fault = np.mean(SNR_fault_array), SNR_normal = np.mean(SNR_normal_array)
     4. Typically, SNR_fault is smaller than SNR_normal.
     """
+
     if np.iscomplexobj(x):
         raise ValueError("`x` must be real.")
 
-    f, t, Sxx = signal.spectrogram(x, fs=fs, nperseg=nperseg, noverlap=noverlap)
-    P = np.max(Sxx, axis=0)  # max power intensity over the entire frequency range at time point t
-    Res = np.sum(Sxx, axis=0) - P  # noise
-    snr = P / Res
+    _, _, power = signal.spectrogram(x, fs=fs, nperseg=nperseg, noverlap=noverlap)
+    p = np.max(power, axis=0)  # max power intensity over the entire frequency range at time point t
+    res = np.sum(power, axis=0) - p  # noise
+    snr = p / res
     return snr
--- tests/signal/test_snr.py
+++ tests/signal/test_snr.py
@@ -3,7 +3,7 @@ from typing import Callable
 import numpy as np
 import pytest
 
-from onebone.signal import snr_feature
+from onebone.signal import snr
 
 
 def generate_signal(fs: float):
@@ -16,13 +16,17 @@ def check_bad_args():
     x = np.array([1.0 + 0.0j])
     fs = 1000.0
     with pytest.raises(ValueError) as ex:
-        snr_feature(x, fs)
+        snr(x, fs)
     assert ex.value.args[0] == "`x` must be real."
 
 
-def check_snr(snr_feature: Callable, fs: float, expected_return: np.float64):
+def check_1d_signal(snr: Callable):
+    pass
+
+
+def check_snr(snr: Callable, fs: float, expected_return: np.float64):
     x = generate_signal(fs)
-    snr = snr_feature(x, fs)
+    snr = snr(x, fs)
     output = np.round(np.mean(snr), 2)
     assert np.all(np.equal(output, expected_return)), (
         f"Wrong return: The expected return is {expected_return}, " + f"but output is {output}"
@@ -31,7 +35,7 @@ def check_snr(snr_feature: Callable, fs: float, expected_return: np.float64):
 
 def test_snr():
     check_bad_args()
-    check_snr(snr_feature, 1000.0, 7.16)
+    check_snr(snr, 1000.0, 7.16)
 
 
 if __name__ == "__main__":
