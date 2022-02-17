5d040c253874851b2c30bf1a766f4d57263f01ff
jeonjuna <88358071+jeonjuna@users.noreply.github.com>
2022-02-17 11:10:16 +0900

    feat: Add fs_crosscorrelation function to onebone.preprocessing (#54)
    * feat: Add fs_crosscorrelation function to onebone.preprocessing
    
    * docs : change doc string
    
    * docs : format change
    
    * docs : change doc string
    
    * docs : change doc string

--- onebone/preprocessing/__init__.py
+++ onebone/preprocessing/__init__.py
@@ -1,3 +1,4 @@
+from .feature_selection import fs_crosscorrelation
 from .scaling import minmax_scaling, zscore_scaling
 
-__all__ = ["minmax_scaling", "zscore_scaling"]
+__all__ = ["minmax_scaling", "zscore_scaling", "fs_crosscorrelation"]
