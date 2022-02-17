989b878b10008e64caabae487ed375add0bb30ec
Kangwhi.Kim <kangwhi.kim@onepredict.com>
2022-02-17 19:15:55 +0900

    feat: n차원 array를 연산 가능하게 변경
    np.fft.fft 함수 이용

--- tests/signal/test_fft.py
+++ tests/signal/test_fft.py
@@ -1,77 +1,91 @@
-from typing import Callable, Tuple
-
 import numpy as np
-import pytest
 
 from onebone.signal import positive_fft
 
 
-def generate_signal(fs: float):
+def generate_signal():
     n = 400
     # sample spacing
-    t = 1.0 / fs
+    t = 1 / 800
     x = np.linspace(0.0, n * t, n, endpoint=False)
     signal = 3 * np.sin(50.0 * 2.0 * np.pi * x) + 2 * np.sin(80.0 * 2.0 * np.pi * x)
     return signal
 
 
-def check_1d_signal(
-    fft: Callable, input_args: Tuple[float, bool, bool], expected_return: np.ndarray
-):
-    fs = input_args[0]
-    signal = generate_signal(fs)
-    f, mag = fft(signal, *input_args)
-    freq = np.around(f[np.where(mag > 2)])
-    assert np.all(np.equal(freq, expected_return)), (
+def check_1d_signal():
+    fs = 800
+    signal = generate_signal()
+    freq, mag = positive_fft(signal, fs=fs, hann=False, normalization=False)
+    target_freq = np.around(freq[np.where(mag > 2)])
+
+    expected_return = np.array([50, 80.0])
+    assert np.all(np.equal(target_freq, expected_return)), (
         f"Wrong return: The expected return is {expected_return}, " + f"but output is {freq}"
     )
 
 
-def check_2d_signal_axis_zero(
-    fft: Callable, input_args: Tuple[float, bool, bool, int], expected_return: np.ndarray
-):
-    fs = input_args[0]
-    signal = generate_signal(fs)
+def check_2d_signal_axis_zero():
+    fs = 800
+    signal = generate_signal()
     signal_2d = np.stack([signal] * 2)
     signal_2d = signal_2d.T
-    f, mag = fft(signal_2d, *input_args)
-    # mag.shape = (n,1)
-    freq = np.around(f[np.where(mag[:, 0] > 2)])
-    assert np.all(np.equal(freq, expected_return)), (
+
+    freq, mag = positive_fft(signal_2d, fs=fs, hann=False, normalization=False, axis=0)
+    target_freq = np.around(freq[np.where(mag[:, 0] > 2)])
+
+    expected_return = np.array([50, 80.0])
+    assert np.all(np.equal(target_freq, expected_return)), (
         f"Wrong return: The expected return is {expected_return}, " + f"but output is {freq}"
     )
 
 
-def check_2d_signal_axis_one(
-    fft: Callable, input_args: Tuple[float, bool, bool, int], expected_return: np.ndarray
-):
-    fs = input_args[0]
-    signal = generate_signal(fs)
+def check_2d_signal_axis_one():
+    fs = 800
+    signal = generate_signal()
     signal_2d = np.stack([signal] * 2)
-    f, mag = fft(signal_2d, *input_args)
-    # mag.shape = (1,n)
-    freq = np.around(f[np.where(mag[0] > 1)])
-    assert np.all(np.equal(freq, expected_return)), (
+
+    freq, mag = positive_fft(signal_2d, fs=fs, hann=False, normalization=True, axis=1)
+    target_freq = np.around(freq[np.where(mag[0, :] > 1)])
+
+    expected_return = np.array([50, 80.0])
+    assert np.all(np.equal(target_freq, expected_return)), (
         f"Wrong return: The expected return is {expected_return}, " + f"but output is {freq}"
     )
 
 
-def check_array_shape(fft: Callable, input_args: Tuple[float, bool, bool]):
-    fs = input_args[0]
-    signal = generate_signal(fs)
+def check_3d_signal():
+    fs = 800
+    signal = generate_signal()
+    signal_2d = np.stack([signal] * 2)
+    signal_3d = np.stack([signal_2d] * 2)
+
+    freq, mag = positive_fft(signal_3d, fs=fs, hann=False, normalization=True, axis=2)
+    expected_return = np.array([50, 80.0])
+
+    for i in range(2):
+        for j in range(2):
+            target_freq = np.around(freq[np.where(mag[i, j] > 1)])
+            assert np.all(np.equal(target_freq, expected_return)), (
+                f"Wrong return: The expected return is {expected_return}, "
+                + f"but output is {freq}"
+            )
+
+
+def check_array_shape():
+    fs = 800
+    signal = generate_signal()
     changed_signal = np.stack([signal] * 3, axis=0)
     changed_signal = np.stack([changed_signal] * 2, axis=0)
-    # case 1: signal shape [2, 3, data_length] -> ArrayShapeError
-    with pytest.raises(ValueError) as ex:
-        fft(changed_signal, *input_args)
-    assert str(ex.value) == "Dimension of signal must be less than 3"
+    # case 1: signal shape (2, 3, data_length) -> ArrayShapeError
+    positive_fft(changed_signal, fs=fs, hann=False, normalization=True)
 
 
 def test_fft():
-    check_1d_signal(positive_fft, (800.0, False, False), np.array([50, 80.0]))
-    check_2d_signal_axis_zero(positive_fft, (800.0, False, False, 0), np.array([50, 80.0]))
-    check_2d_signal_axis_one(positive_fft, (800.0, False, True, 1), np.array([50, 80.0]))
-    check_array_shape(positive_fft, (800.0, False, True))
+    check_1d_signal()
+    check_2d_signal_axis_zero()
+    check_2d_signal_axis_one()
+    check_3d_signal()
+    check_array_shape()
 
 
 if __name__ == "__main__":
