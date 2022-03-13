5fa013f8ecebd01375fae2e3849b474aa87cf670
SunjinKim <sunjin.kim@onepredict.com>
2022-03-14 00:03:11 +0900

    modify_test.py

--- tests/signal/test_hampel_filter.py
+++ tests/signal/test_hampel_filter.py
@@ -10,7 +10,7 @@ from numpy.testing import assert_array_almost_equal
 from onebone.signal import hampel_filter
 
 
-def timeseries_data(num_outlier: int = 4) -> np.ndarray:
+def timeseries_data(num_outlier: int = 10) -> np.ndarray:
 
     """
     make example timeseries
@@ -20,22 +20,29 @@ def timeseries_data(num_outlier: int = 4) -> np.ndarray:
     ----------
     num_outlier : numpy.ndarray
         You can set the number of random outliers.
-        default = 4
+        default = 10
     """
-    t = np.linspace(0, 1, 100)
+    t = np.linspace(0, 1, 1000)
     y = np.sin(2 * np.pi * 10 * t)
-    make_outlier = np.random.randint(0, 100, size=num_outlier)
-    np.put(y, [make_outlier], 4)
+    make_outlier = np.random.randint(0, 1000, size=num_outlier)
+    np.put(y, [make_outlier], 10)
 
     return y
 
 
 def test_hampel_filter(is_plot: bool = False):
-    filtered_data = hampel_filter.hampel_filter(timeseries_data(), 2)[0]
+    window_size = 4
+    filtered_data = hampel_filter.hampel_filter(timeseries_data(), window_size)[0]
     origin_data = timeseries_data()
     perfect_data = timeseries_data(num_outlier=0)
 
-    assert_array_almost_equal(filtered_data, perfect_data, decimal=0)
+    check_window_region = [window_size * 2, len(timeseries_data()) - window_size * 2]
+
+    assert_array_almost_equal(
+        filtered_data[check_window_region[0] : check_window_region[1]],
+        perfect_data[check_window_region[0] : check_window_region[1]],
+        decimal=0,
+    )
 
     if is_plot:
         import matplotlib.pyplot as plt
